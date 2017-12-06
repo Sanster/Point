cb83dbb615390672f61670f156269cede052e949
weiqing chu <cwq@CHU-2.local>
2017-12-06 16:21:30 +0800

    make scale image work

--- app/src/main/java/com/sanster/point/ZoomImageView.kt
+++ app/src/main/java/com/sanster/point/ZoomImageView.kt
@@ -2,13 +2,17 @@ package com.sanster.point
 
 import android.content.Context
 import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.graphics.Rect
 import android.support.v7.widget.AppCompatImageView
 import android.util.AttributeSet
+import android.util.Log
 import android.view.MotionEvent
 import android.view.ScaleGestureDetector
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.target.SimpleTarget
 import com.bumptech.glide.request.transition.Transition
+import com.sanster.point.utils.Constants
 
 class ZoomImageView(context: Context, attrs: AttributeSet) : AppCompatImageView(context, attrs) {
     private val MIN_SCALE_FACTOR = 1.0F
@@ -17,17 +21,30 @@ class ZoomImageView(context: Context, attrs: AttributeSet) : AppCompatImageView(
     private var mActiveY: Float = 0F
     private var mMoveX: Float = 0F
     private var mMoveY: Float = 0F
-    private var mScaleFactor: Float = 1.0F
+    private var mScale: Float = 1.0F
+    private var mScaleCenterX: Float = 0F
+    private var mScaleCenterY: Float = 0F
+
+    private var mCanvasClipsBounds: Rect = Rect()
+    private val mScaleGestureDetector: ScaleGestureDetector
+
+    init {
+        mScaleGestureDetector = ScaleGestureDetector(this.context, ScaleListener())
+    }
 
     private inner class ScaleListener : ScaleGestureDetector.SimpleOnScaleGestureListener() {
         override fun onScale(detector: ScaleGestureDetector): Boolean {
-            mScaleFactor *= detector.scaleFactor
+            mScale *= detector.scaleFactor
 
-            mScaleFactor = Math.max(MIN_SCALE_FACTOR, Math.min(mScaleFactor, MAX_SCALE_FACTOR))
+            mScale = Math.max(MIN_SCALE_FACTOR, Math.min(mScale, MAX_SCALE_FACTOR))
 
             // 缩放的焦点处于屏幕坐标系中
-            mActiveX = detector.focusX
-            mActiveY = detector.focusY
+            mScaleCenterX = detector.focusX
+            mScaleCenterY = detector.focusY
+
+            invalidate()
+
+            Log.d(Constants.TAG, "scale: $mScale scaleCenterX: $mScaleCenterX scaleCenterY: $mScaleCenterY")
             return true
         }
     }
@@ -44,6 +61,10 @@ class ZoomImageView(context: Context, attrs: AttributeSet) : AppCompatImageView(
     }
 
     override fun onTouchEvent(event: MotionEvent): Boolean {
+        var handled = false
+
+        handled = mScaleGestureDetector.onTouchEvent(event)
+
         when (event.action) {
             MotionEvent.ACTION_DOWN -> {
                 mActiveX = event.x
@@ -55,8 +76,17 @@ class ZoomImageView(context: Context, attrs: AttributeSet) : AppCompatImageView(
             }
 
         }
-        return super.onTouchEvent(event)
+
+        return handled
     }
 
+    override fun onDraw(canvas: Canvas) {
+        Log.d(Constants.TAG, "onDraw")
+        canvas.save()
+        canvas.getClipBounds(mCanvasClipsBounds)
+        canvas.scale(mScale, mScale, mScaleCenterX + mCanvasClipsBounds.left, mScaleCenterY + mCanvasClipsBounds.top)
+        super.onDraw(canvas)
+        canvas.restore()
+    }
 }
 
