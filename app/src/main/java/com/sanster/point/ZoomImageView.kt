0713772d83a52c3f9f6e1911ea99a8d06d72a0a7
weiqing chu <cwq@CHU-2.local>
2017-12-10 14:50:14 +0800

    fixscale not in center

--- app/src/main/java/com/sanster/point/ZoomImageView.kt
+++ app/src/main/java/com/sanster/point/ZoomImageView.kt
@@ -3,7 +3,6 @@ package com.sanster.point
 import android.content.Context
 import android.graphics.Bitmap
 import android.graphics.Matrix
-import android.graphics.Rect
 import android.support.v7.widget.AppCompatImageView
 import android.util.AttributeSet
 import android.util.Log
@@ -12,11 +11,14 @@ import android.view.ScaleGestureDetector
 import android.view.View
 import com.sanster.point.utils.Constants
 import android.graphics.RectF
-import android.graphics.drawable.Drawable
 
 
+/**
+ * The code for this class was adapted from the PhotoView project: https://github.com/chrisbanes/PhotoView
+ */
 class ZoomImageView(context: Context, attrs: AttributeSet) : AppCompatImageView(context, attrs), ScaleGestureDetector.OnScaleGestureListener, View.OnTouchListener {
-    private val MAX_SCALE_FACTOR = 3.0F
+    private val mMaxScale: Float = 3.0F
+
     private var mActiveX: Float = 0F
     private var mActiveY: Float = 0F
     private var mMoveX: Float = 0F
@@ -25,12 +27,13 @@ class ZoomImageView(context: Context, attrs: AttributeSet) : AppCompatImageView(
     private var mScaleCenterX: Float = 0F
     private var mScaleCenterY: Float = 0F
 
-    private val mScaleMatrix: Matrix = Matrix()
-    private val mTranMatrix: Matrix = Matrix()
+    private var mBaseMatrix: Matrix = Matrix()
+    private val mSuppMatrix: Matrix = Matrix()
     private var mDrawMatrix: Matrix = Matrix()
-    private val mMatrixValues = FloatArray(9)
 
-    private var mCanvasClipsBounds: Rect = Rect()
+    private val mMatrixValues = FloatArray(9)
+    private val mDisplayRect = RectF()
+
     private val mScaleGestureDetector: ScaleGestureDetector
 
     init {
@@ -47,26 +50,20 @@ class ZoomImageView(context: Context, attrs: AttributeSet) : AppCompatImageView(
     }
 
     override fun onScale(detector: ScaleGestureDetector): Boolean {
-        val scale = getScale()
-        var scaleFactor = detector.scaleFactor
+        val scaleFactor = detector.scaleFactor
+        if (scaleFactor.isNaN() || scaleFactor.isInfinite()) return false
 
         if (drawable == null) return true
 
-        if (scale in mInitScale..MAX_SCALE_FACTOR) {
-            val s = scaleFactor * scale
+        val scale = getScale()
 
-            scaleFactor = when {
-                s < mInitScale -> mInitScale / scale
-                s > MAX_SCALE_FACTOR -> MAX_SCALE_FACTOR / scale
-                else -> scaleFactor
-            }
-
-            mScaleMatrix.postScale(scaleFactor, scaleFactor, detector.focusX, detector.focusY)
-            checkBorder()
-            updateImageMatrix()
+        if ((scale < mMaxScale || scaleFactor < 1f)
+                && (scale > mInitScale || scaleFactor > 1f)) {
+            mSuppMatrix.postScale(scaleFactor, scaleFactor, detector.focusX, detector.focusY)
+            checkAndDisplayMatrix()
         }
 
-        Log.d(Constants.TAG, "scale: $scale scaleCenterX: $mScaleCenterX scaleCenterY: $mScaleCenterY")
+        Log.d(Constants.TAG, "scale: ${getScale()} scaleCenterX: $mScaleCenterX scaleCenterY: $mScaleCenterY")
 
         return true
     }
@@ -91,91 +88,104 @@ class ZoomImageView(context: Context, attrs: AttributeSet) : AppCompatImageView(
         return handled
     }
 
+
     override fun setImageBitmap(bm: Bitmap) {
+        mBaseMatrix.reset()
+
         // 拿到图片的宽和高
         val dw = bm.width
         val dh = bm.height
 
-        // 如果图片的宽/高大于屏幕，则缩放至屏幕的宽/高
-        mInitScale = when {
-            dw > width && dh <= height -> width * 1.0f / dw
-            dh > height && dw <= width -> height * 1.0f / dh
-            dw > width && dh > height -> Math.min(width * 1.0f / dw, height * 1.0f / dh)
-            else -> mInitScale
-        }
+//        // 如果图片的宽/高大于屏幕，则缩放至屏幕的宽/高
+//        mInitScale = when {
+//            dw > width && dh <= height -> width * 1.0f / dw
+//            dh > height && dw <= width -> height * 1.0f / dh
+//            dw > width && dh > height -> Math.min(width * 1.0f / dw, height * 1.0f / dh)
+//            else -> mInitScale
+//        }
+//
+//        // 图片移动至屏幕中心
+//        mBaseMatrix.postTranslate((width - dw) / 2.0f, (height - dh) / 2.0f)
+//        mBaseMatrix.postScale(mInitScale, mInitScale, width / 2.0f, height / 2.0f)
 
-        // 图片移动至屏幕中心
-        mTranMatrix.postTranslate((width - dw) / 2.0f, (height - dh) / 2.0f)
-        mScaleMatrix.postScale(mInitScale, mInitScale, width / 2.0f, height / 2.0f)
+        val mTempSrc = RectF(0f, 0f, dw.toFloat(), dh.toFloat())
+        val mTempDst = RectF(0f, 0f, width.toFloat(), height.toFloat())
+        mBaseMatrix.setRectToRect(mTempSrc, mTempDst, Matrix.ScaleToFit.CENTER)
 
-        updateImageMatrix()
+        updateImageViewMatrix()
 
         super.setImageBitmap(bm)
     }
 
     fun getScale(): Float {
-        mDrawMatrix.getValues(mMatrixValues)
+        mSuppMatrix.getValues(mMatrixValues)
         return mMatrixValues[Matrix.MSCALE_X]
     }
 
-    private fun updateImageMatrix() {
-        mDrawMatrix.reset()
-        mDrawMatrix.postConcat(mTranMatrix)
-        mDrawMatrix.postConcat(mScaleMatrix)
-        imageMatrix = mDrawMatrix
+    /**
+     * 在缩放时，进行图片显示范围的控制，防止图片的周围出现白边
+     */
+    private fun checkAndDisplayMatrix() {
+        if (checkMatrixBounds()) {
+            updateImageViewMatrix()
+        }
+    }
+
+    private fun checkMatrixBounds(): Boolean {
+        val rect = getDisplayRect(getDrawMatrix()) ?: return false
+
+        var deltaX: Float
+        var deltaY: Float
+
+        Log.d(Constants.TAG, rect.toString())
+        Log.d(Constants.TAG, "rect.width: ${rect.width()}, rect.height: ${rect.height()}")
+        Log.d(Constants.TAG, "width: $width, height: $height")
+
+        deltaX = when {
+            rect.width() <= width -> (width - rect.width()) / 2.0f - rect.left
+            rect.left > 0 -> -rect.left
+            rect.right < width -> width - rect.right
+            else -> 0f
+        }
+
+        deltaY = when {
+            rect.height() <= height -> (height - rect.height()) / 2.0f - rect.top
+            rect.top > 0 -> -rect.top
+            rect.bottom < height -> height - rect.bottom
+            else -> 0f
+        }
+
+        Log.d(Constants.TAG, "deltaX: $deltaX, deltaY: $deltaY")
+
+        mSuppMatrix.postTranslate(deltaX, deltaY)
+        updateImageViewMatrix()
+
+        return true
     }
 
     /**
-     * 在缩放时，进行图片显示范围的控制
+     * Helper method that maps the supplied Matrix to the current Drawable
+     *
+     * @param matrix - Matrix to map Drawable against
+     * @return RectF - Displayed Rectangle
      */
-    private fun checkBorder() {
-        val rect = getMatrixRectF()
-        var deltaX = 0f
-        var deltaY = 0f
-
-        // TODO: 重构这里的代码，看懂这边的逻辑
-        // TODO: 如果快速缩小图片，图片的两边还是会有白边，并且不居中，看下为什么
-        // 如果宽或高大于屏幕，则控制范围
-        if (rect.width() >= width) {
-            if (rect.left > 0) {
-                deltaX = -rect.left
-            }
-            if (rect.right < width) {
-                deltaX = width - rect.right
-            }
-        }
-        if (rect.height() >= height) {
-            if (rect.top > 0) {
-                deltaY = -rect.top
-            }
-            if (rect.bottom < height) {
-                deltaY = height - rect.bottom
-            }
-        }
-
-        // 如果宽或高小于屏幕，则让其居中
-        if (rect.width() < width) {
-            deltaX = width * 0.5f - rect.right + 0.5f * rect.width()
-        }
-        if (rect.height() < height) {
-            deltaY = height * 0.5f - rect.bottom + 0.5f * rect.height()
-        }
-
-        mTranMatrix.postTranslate(deltaX, deltaY)
-    }
-
-    /**
-     * 根据当前图片的 Matrix 获得图片的范围
-     * 返回的 RectF 的 top 和 left 可以为负数
-     */
-    private fun getMatrixRectF(): RectF {
-        val matrix = mDrawMatrix
-        val rect = RectF()
+    private fun getDisplayRect(matrix: Matrix): RectF? {
         if (null != drawable) {
-            rect.set(0f, 0f, drawable.intrinsicWidth.toFloat(), drawable.intrinsicHeight.toFloat())
-            matrix.mapRect(rect)
+            mDisplayRect.set(0f, 0f, drawable.intrinsicWidth.toFloat(), drawable.intrinsicHeight.toFloat())
+            matrix.mapRect(mDisplayRect)
+            return mDisplayRect
         }
-        return rect
+        return null
+    }
+
+    private fun updateImageViewMatrix() {
+        imageMatrix = getDrawMatrix()
+    }
+
+    private fun getDrawMatrix(): Matrix {
+        mDrawMatrix.set(mBaseMatrix)
+        mDrawMatrix.postConcat(mSuppMatrix)
+        return mDrawMatrix
     }
 }
 
